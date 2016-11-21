b41faf73059ee02bfec6bcf68f0440ef35a9aabc
Vincenzo Di Somma <vincenzo.disomma@gmail.com>
2016-11-21 10:34:13 +0100

    Adding comment to data structs

--- Exercise2/solution/tedfeed/feed.go
+++ Exercise2/solution/tedfeed/feed.go
@@ -2,11 +2,13 @@ package tedfeed
 
 import "encoding/xml"
 
+// Link maps a subset of the atom link element
 type Link struct {
 	Rel  string `xml:"rel,attr"`
 	HRef string `xml:"href,attr"`
 }
 
+// Entry maps a subset of the atom entry element
 type Entry struct {
 	Id          string `xml:"id"`
 	TalkId      string `xml:"ted:talkid"`
@@ -19,6 +21,7 @@ type Entry struct {
 	Summary     string `xml:"summary"`
 }
 
+// Feed maps a subset of the main atom feed element
 type Feed struct {
 	XMLName xml.Name `xml:"feed"`
 	Updated string   `xml:"updated"`
